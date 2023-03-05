c7b1968cd827fca0ef566b1ac67443d05cf58469
Raul Avila Catala <cl2raul66@hotmail.com>
2023-03-05 13:44:33 -0600

    tienda

---
+++ src/AgroserviciosTienda/AgroserviciosTienda/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AgroserviciosTienda;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/AgroserviciosTienda/AgroserviciosTienda/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AgroserviciosTienda;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
