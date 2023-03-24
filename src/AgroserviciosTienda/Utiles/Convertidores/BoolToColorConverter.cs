50b003288166cba091d8ac3c1500b50f1fc14ce4
Raul Avila Catala <cl2raul66@hotmail.com>
2023-03-23 23:27:32 -0600

    reajustes en UI, agregar vista para precios de venta, también agregar productos de entrada aparte como se hizo en ventas

---
+++ src/AgroserviciosTienda/Utiles/Convertidores/BoolToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace AgroserviciosTienda.Utiles.Convertidores;
+
+public class BoolToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool boolValue = (bool)value;
+        string hexColor = boolValue ? "#DFD8F7" : "#512BD4";
+        return Color.FromArgb(hexColor);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ src/AgroserviciosTienda/Utiles/Convertidores/ProductosVentaTotalConverter.cs
@@ -0,0 +1,29 @@
+﻿using AgroserviciosTienda.Modelos;
+using System.Globalization;
+
+namespace AgroserviciosTienda.Utiles.Convertidores;
+
+public class ProductosVentaTotalConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not List<ProductoVenta> productos)
+        {
+            return null;
+        }
+
+        decimal total = 0;
+
+        foreach (ProductoVenta producto in productos)
+        {
+            total += producto.Cantidad * producto.Precio;
+        }
+
+        return total.ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
